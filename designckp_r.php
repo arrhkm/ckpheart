b9fcc46a25c07ae5b15b6d254d216c661f85f7cb
hakam <arrafhakam@gmail.com>
2015-12-21 15:04:20 +0700

    test

---
+++ designckp_r.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+<style type="text/css">
+<!--
+.style2 {
+	font-family: Arial, Helvetica, sans-serif;
+	font-weight: bold;
+}
+.style10 {font-size: 11pt; font-family: Arial, Helvetica, sans-serif; }
+.style12 {font-family: Arial, Helvetica, sans-serif}
+.style16 {font-size: 11pt; }
+.style17 {font-size: 11pt; font-family: Arial, Helvetica, sans-serif; font-weight: bold; }
+-->
+</style>
+</head>
+
+<body>
+<div align="right" class="style2">
+  <table width="100" border="1">
+    <tr>
+      <td><div align="center">CKP-R</div></td>
+    </tr>
+  </table>
+  
+  <p align="center">CAPAIAN KINERJA PEGAWAI TAHUN tes </p>
+</div>
+<table width="442" border="0">
+  <tr>
+    <td width="169"><span class="style10">Satuan Organisasi </span></td>
+    <td width="7"><span class="style10">:</span></td>
+    <td width="252"><span class="style10">tes</span></td>
+  </tr>
+  <tr>
+    <td><span class="style10">Nama</span></td>
+    <td>:</td>
+    <td><span class="style10">tes</span></td>
+  </tr>
+  <tr>
+    <td><span class="style10">Jabatan</span></td>
+    <td>:</td>
+    <td><span class="style10">tes</span></td>
+  </tr>
+  <tr>
+    <td><span class="style10">Periode</span></td>
+    <td>:</td>
+    <td><span class="style10">tes</span></td>
+  </tr>
+</table>
+<table width="1095" border="1">
+  <tr>
+    <td width="36" rowspan="2"><div align="center" class="style16"><span class="style12">No</span></div></td>
+    <td width="321" rowspan="2"><div align="center" class="style16"><span class="style12">Uraian Kegiatan </span></div></td>
+    <td width="73" rowspan="2"><div align="center" class="style16"><span class="style12">Satuan</span></div></td>
+    <td colspan="3"><div align="center" class="style16"><span class="style12">Kuantitas</span></div></td>
+    <td width="85" rowspan="2"><div align="center" class="style16"><span class="style12">Tingkat Kualitas (%) </span></div></td>
+    <td width="85" rowspan="2"><div align="center" class="style16"><span class="style12">Kode Butir Kegiatan </span></div></td>
+    <td width="85" rowspan="2"><div align="center" class="style16"><span class="style12">Angka Kredit </span></div></td>
+    <td width="99" rowspan="2"><div align="center" class="style16"><span class="style12">Keterangan</span></div></td>
+  </tr>
+  <tr>
+    <td width="61"><div align="center" class="style10">Target</div></td>
+    <td width="85"><div align="center" class="style10">Realisasi</div></td>
+    <td width="85"><div align="center" class="style10">%</div></td>
+  </tr>
+  <tr>
+    <td class="style10"><div align="center">(1)</div></td>
+    <td class="style10"><div align="center">(2)</div></td>
+    <td class="style10"><div align="center">(3)</div></td>
+    <td class="style10"><div align="center">(4)</div></td>
+    <td class="style10"><div align="center">(5)</div></td>
+    <td class="style10"><div align="center">(6)</div></td>
+    <td class="style10"><div align="center">(7)</div></td>
+    <td class="style10"><div align="center">(8)</div></td>
+    <td class="style10"><div align="center">(9)</div></td>
+    <td class="style10"><div align="center">(10)</div></td>
+  </tr>
+  <tr>
+    <td colspan="2"><strong class="style10">UTAMA</strong></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><span class="style10">tesno</span></td>
+    <td><span class="style10">teskegiatan</span></td>
+    <td><span class="style10">tessatuan</span></td>
+    <td><span class="style10">testarget</span></td>
+    <td><span class="style10">tesrealisasi</span></td>
+    <td><span class="style10">tespersen</span></td>
+    <td><span class="style10">teskualitas</span></td>
+    <td><span class="style10">teskodebutir</span></td>
+    <td><span class="style10">teskredit</span></td>
+    <td><span class="style10">tesketerangan</span></td>
+  </tr>
+  <tr>
+    <td colspan="2"><strong class="style10">TAMBAHAN</strong></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><span class="style10">tesno</span></td>
+    <td><span class="style10">teskegiatan</span></td>
+    <td><span class="style10">tessatuan</span></td>
+    <td><span class="style10">testarget</span></td>
+    <td><span class="style10">tesrealisasi</span></td>
+    <td><span class="style10">tespersen</span></td>
+    <td><span class="style10">teskualitas</span></td>
+    <td><span class="style10">teskodebutir</span></td>
+    <td><span class="style10">teskredit</span></td>
+    <td><span class="style10">tesketerangan</span></td>
+  </tr>
+  <tr>
+    <td colspan="8"><div align="center" class="style17">JUMLAH</div></td>
+    <td><span class="style10">tesjumlah</span></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="5" class="style10"><div align="center"><strong>RATA-RATA</strong></div></td>
+    <td><span class="style10">tesrata1</span></td>
+    <td><span class="style10">tesrata2</span></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="5" class="style17"><div align="center">CAPAIAN KINERJA PEGAWAI (CKP) </div></td>
+    <td colspan="2"><span class="style10">tesckp</span></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<table width="280" border="0">
+  <tr>
+    <td width="239" class="style10">Penilaian Kinerja </td>
+  </tr>
+  <tr>
+    <td class="style10">Tanggal : tes tanggal </td>
+  </tr>
+</table>
+<table width="1092" border="0">
+  <tr>
+    <td width="520" class="style10"><div align="center">Pegawai Yang Dinilai </div></td>
+    <td width="562" class="style10"><div align="center">Pejabat Penilai </div></td>
+  </tr>
+  <tr>
+    <td class="style10"><div align="center"></div></td>
+    <td class="style10"><div align="center"></div></td>
+  </tr>
+  <tr>
+    <td class="style10"><div align="center"></div></td>
+    <td class="style10"><div align="center"></div></td>
+  </tr>
+  <tr>
+    <td class="style10"><div align="center">(tesnama)</div></td>
+    <td class="style10"><div align="center">(tesnama)</div></td>
+  </tr>
+  <tr>
+    <td class="style10"><div align="center">NIP. tesnip </div></td>
+    <td class="style10"><div align="center">NIP. tesnip </div></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+</body>
+</html>
